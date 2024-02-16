31bbfe99130931244c1d450fb847f4a0ad98c52b
DavidOdewusi1 <dodewusi@digicoreltd.com>
2024-02-16 12:04:58 +0100

    First commit

---
+++ src/test/java/com/example/eatandgo/EatAndGooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatandgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatAndGooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
