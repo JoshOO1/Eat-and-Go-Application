4452b97a96133f173a5f373180c20f93527855c8
Ojegbile Joshua <joshuaojegbile11@gmail.com>
2024-02-20 09:00:03 +0100

    CI-CD-Pipeline-First-Commit

---
+++ src/test/java/com/EatandGoAppplication/EatandGoApplication/EatAndGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EatandGoAppplication.EatandGoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatAndGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
